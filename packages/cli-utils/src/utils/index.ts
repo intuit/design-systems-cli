545df2487c66fe1716429ad0f2111feb5ad1d295
mishavp2001 <mishavp2001@yahoo.com>
2020-02-13 08:49:15 -0800

    Update index.ts

--- packages/cli-utils/src/utils/index.ts
+++ packages/cli-utils/src/utils/index.ts
@@ -1,5 +1,5 @@
 export { default as lerna } from './lerna';
-export { default as getRepoRoot } from './get-monorepo-root';
+export { default as getRepoRoot } from './get-repo-root';
 export { default as getMonorepoRoot } from './get-monorepo-root';
 export { default as monorepoName } from './monorepo-name';
 export { default as loadUserWebpackConfig } from './load-user-webpack-config';
