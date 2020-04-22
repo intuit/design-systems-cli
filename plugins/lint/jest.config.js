479847d7887bc14b7d3a0dfe6e4d3a6892a03283
Raj Vasikarla <raj_vasikarla@intuit.com>
2020-04-21 18:11:31 -0700

    refactor: organizing code and writing unit test

---
+++ plugins/lint/jest.config.js
@@ -0,0 +1,12 @@
+const base = require('@design-systems/test/jest.config.base');
+const { name } = require('./package.json');
+
+module.exports = {
+  ...base,
+  name,
+  displayName: name,
+  testPathIgnorePatterns: [
+    ...(base.testPathIgnorePatterns || []),
+    'test-plugin'
+  ]
+};
