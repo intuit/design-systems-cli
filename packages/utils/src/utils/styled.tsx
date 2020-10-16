1204e8c5b4acc2074d91433231b0716aa05eda96
Kendall_Gassner@intuit.com <Kendall_Gassner@intuit.com>
2020-10-16 10:33:57 -0700

    change type

--- packages/utils/src/utils/styled.tsx
+++ packages/utils/src/utils/styled.tsx
@@ -49,7 +49,7 @@ interface WrappedComponent {
  * }
  */
 export function styled<T extends keyof JSX.IntrinsicElements>(
-  element:  T | [T, React.ReactElement[]],
+  element:  T | [T, React.ReactNode[]],
   options: string | WrappedComponent
 ) {
   const defaultDescription = `This component accepts all HTML attributes for a "${element}" element.`;
