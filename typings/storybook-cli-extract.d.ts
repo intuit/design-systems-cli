cb8bbfef1f63d0db5f0069be5a19cd7813efaec9
Harris Borawski <harris_borawski@intuit.com>
2021-05-05 16:41:47 -0700

    remove commented out code

--- typings/storybook-cli-extract.d.ts
+++ typings/storybook-cli-extract.d.ts
@@ -1,6 +1,3 @@
-// declare module '@storybook/cli/dist/cjs/extract' {
-//   export function extract(input: string, targetPath: string): Promise<void>;
-// }
 declare module '@storybook/cli/dist/cjs/extract' {
   export * from '@storybook/cli/dist/ts3.9/extract'
 }
